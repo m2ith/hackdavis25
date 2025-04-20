4faa49e19750e3b6d72823d46e0881a036c90a6d
Meredith Whisman <mwhismain@gmail.com>
2025-04-20 00:44:23 -0700

    updated index and styles, added script file

---
+++ script.js
@@ -0,0 +1,27 @@
+(function () {
+    "use strict";
+    console.log("reading JS");
+
+    // TYPEIT ANIMATIONS
+
+    // Section 1
+    new TypeIt("#section1", {
+        strings: "What is food insecurity?",
+        speed: 50,
+        waitUntilVisible: true,
+    }).go();
+
+    // Section 2
+    new TypeIt("#section2", {
+        strings: "Why does this happen . . . ?",
+        speed: 50,
+        waitUntilVisible: true,
+    }).go();
+
+    // Section 6
+    new TypeIt("#section6", {
+        strings: "Why does this matter?",
+        speed: 50,
+        waitUntilVisible: true,
+    }).go();
+})();
